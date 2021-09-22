fe1d70e069c07226c28768f9bac6f3b0228b4d6d
Alejandro Peña López <alejandro.pelopez98@gmail.com>
2021-09-21 18:38:26 -0700

    feat: organize publish settings, like connections strings and end points in client

--- angularClient/src/environments/environment.prod.ts
+++ angularClient/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://localhost:44350/api/'
-  apiUrl: 'http://diracessence-002-site2.itempurl.com/api/',
+  apiUrl: 'https://localhost:44350/api/'
+  // apiUrl: 'http://diracessence-002-site2.itempurl.com/api/',
 };
--- angularClient/src/environments/environment.ts
+++ angularClient/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://localhost:44350/api/',
-  apiUrl: 'http://diracessence-002-site2.itempurl.com/api/',
+  apiUrl: 'https://localhost:44350/api/',
+  // apiUrl: 'http://diracessence-002-site2.itempurl.com/api/',
 };
 
 /*
